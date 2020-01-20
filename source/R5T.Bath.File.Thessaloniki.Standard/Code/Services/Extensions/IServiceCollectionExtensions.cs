b0f78c80505f7cdbcb9b5bcdb062c93e9bce03c1
MinexAutomation <minex.automation@gmail.com>
2020-01-19 18:23:50 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Bath.File.Thessaloniki.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Bath.File.Default;
+using R5T.Thessaloniki.Standard;
+
+
+namespace R5T.Bath.File.Thessaloniki.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<IHumanOutputFileDirectoryPathProvider, TemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider>()
+                .AddCTempTemporaryDirectoryPathProvider()
+                ;
+
+            return services;
+        }
+    }
+}
