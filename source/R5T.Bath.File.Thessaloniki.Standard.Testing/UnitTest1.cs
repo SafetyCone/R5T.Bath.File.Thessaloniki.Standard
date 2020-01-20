230e16a0cd1076891e7eb07f8e5c819ba76c0245
MinexAutomation <minex.automation@gmail.com>
2020-01-19 18:03:17 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Bath.File.Thessaloniki.Standard.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Bath.File.Thessaloniki.Standard.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ source/R5T.Bath.File.Thessaloniki.Standard/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Bath.File.Thessaloniki.Standard
+{
+    public class Class1
+    {
+    }
+}
