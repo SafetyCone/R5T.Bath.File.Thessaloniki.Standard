4f419dfc3c10533e7cd5a0a5e2a70096c0504967
MinexAutomation <minex.automation@gmail.com>
2020-01-19 19:08:20 -0800

    Added IServiceCollection extensions to add IHumanOutputFilePathProvider services.

--- source/R5T.Bath.File.Thessaloniki.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Bath.File.Thessaloniki.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -3,6 +3,8 @@
 using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Bath.File.Default;
+using R5T.Dacia.Extensions;
+using R5T.Lombardy;
 using R5T.Thessaloniki.Standard;
 
 
@@ -16,6 +18,28 @@ namespace R5T.Bath.File.Thessaloniki.Standard
                 .AddSingleton<IHumanOutputFileDirectoryPathProvider, TemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider>()
                 .AddCTempTemporaryDirectoryPathProvider()
                 ;
+
+            return services;
+        }
+
+        public static IServiceCollection AddTemporaryDirectoryBasedHumanOutputFilePathProvider<THumanOutputFileNameProvider, TStringlyTypedPathOperator>(this IServiceCollection services)
+            where THumanOutputFileNameProvider: class, IHumanOutputFileNameProvider
+            where TStringlyTypedPathOperator: class, IStringlyTypedPathOperator
+        {
+            services
+                .AddSingleton<IHumanOutputFilePathProvider, DefaultHumanOutputFilePathProvider>()
+                .AddTemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider()
+                .TryAddSingletonFluent<IHumanOutputFileNameProvider, THumanOutputFileNameProvider>()
+                .TryAddSingletonFluent<IStringlyTypedPathOperator, TStringlyTypedPathOperator>()
+                ;
+
+            return services;
+        }
+
+        public static IServiceCollection AddTemporaryDirectoryBasedHumanOutputFilePathProvider<TStringlyTypedPathOperator>(this IServiceCollection services)
+            where TStringlyTypedPathOperator : class, IStringlyTypedPathOperator
+        {
+            services.AddTemporaryDirectoryBasedHumanOutputFilePathProvider<DefaultHumanOutputFileNameProvider, TStringlyTypedPathOperator>();
 
             return services;
         }
