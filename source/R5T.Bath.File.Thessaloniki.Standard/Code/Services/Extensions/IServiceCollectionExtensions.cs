34d6f90451eae6d6e96cccdf35da2c6432788959
MinexAutomation <minex.automation@gmail.com>
2020-01-30 02:08:33 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Bath.File.Thessaloniki.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Bath.File.Thessaloniki.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -3,48 +3,76 @@
 using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Bath.File.Default;
-using R5T.Dacia.Extensions;
-using R5T.Lombardy;
-using R5T.Thessaloniki.Standard;
+using R5T.Dacia;
+using R5T.Lombardy.Standard;
+using R5T.Thessaloniki.CDrive;
 
 
 namespace R5T.Bath.File.Thessaloniki.Standard
 {
     public static class IServiceCollectionExtensions
     {
-        public static IServiceCollection AddHumanOutputFileDirectoyPathProvider_TemporaryDirectoryBased(this IServiceCollection services)
+        /// <summary>
+        /// Adds a C-drive-based <see cref="IHumanOutputFileDirectoryPathProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddCDriveHumanOutputFileDirectoryPathProvider(this IServiceCollection services)
         {
-            services
-                .AddSingleton<IHumanOutputFileDirectoryPathProvider, TemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider>()
-                .AddCTempTemporaryDirectoryPathProvider()
-                ;
+            services.AddTemporaryDirectoryBasedHumanOutputFileDirectoryPathProvider(services.AddTemporaryDirectoryPathProviderAction());
 
             return services;
         }
 
-        public static IServiceCollection AddHumanOutputFilePathProvider_TemporaryDirectoryBased<THumanOutputFileNameProvider, TStringlyTypedPathOperator>(this IServiceCollection services)
-            where THumanOutputFileNameProvider: class, IHumanOutputFileNameProvider
-            where TStringlyTypedPathOperator: class, IStringlyTypedPathOperator
+        /// <summary>
+        /// Adds a C-drive-based <see cref="IHumanOutputFileDirectoryPathProvider"/> service.
+        /// </summary>
+        public static ServiceAction<IHumanOutputFileDirectoryPathProvider> AddCDriveHumanOutputFileDirectoryPathProviderAction(this IServiceCollection services)
         {
-            services
-                .AddSingleton<IHumanOutputFilePathProvider, DefaultHumanOutputFilePathProvider>()
-                .AddHumanOutputFileDirectoyPathProvider_TemporaryDirectoryBased()
-                .TryAddSingletonFluent<IHumanOutputFileNameProvider, THumanOutputFileNameProvider>()
-                .TryAddSingletonFluent<IStringlyTypedPathOperator, TStringlyTypedPathOperator>()
+            var serviceAction = new ServiceAction<IHumanOutputFileDirectoryPathProvider>(() => services.AddCDriveHumanOutputFileDirectoryPathProvider());
+            return serviceAction;
+        }
+
+        /// <summary>
+        /// Adds a C-drive-based <see cref="IHumanOutputFilePathProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddCDriveHumanOutputFilePathProvider(this IServiceCollection services,
+            ServiceAction<IHumanOutputFileNameProvider> addHumanOutputFileNameProvider)
+        {
+            services.AddDefaultHumanOutputFilePathProvider(
+                services.AddCDriveHumanOutputFileDirectoryPathProviderAction(),
+                addHumanOutputFileNameProvider,
+                services.AddStringlyTypedPathOperatorAction())
                 ;
 
             return services;
         }
 
         /// <summary>
-        /// Uses the <see cref="DefaultHumanOutputFileNameProvider"/> service.
+        /// Adds a C-drive-based <see cref="IHumanOutputFilePathProvider"/> service.
         /// </summary>
-        public static IServiceCollection AddHumanOutputFilePathProvider_TemporaryDirectoryBased<TStringlyTypedPathOperator>(this IServiceCollection services)
-            where TStringlyTypedPathOperator : class, IStringlyTypedPathOperator
+        public static ServiceAction<IHumanOutputFilePathProvider> AddCDriveHumanOutputFilePathProviderAction(this IServiceCollection services,
+            ServiceAction<IHumanOutputFileNameProvider> addHumanOutputFileNameProvider)
         {
-            services.AddHumanOutputFilePathProvider_TemporaryDirectoryBased<DefaultHumanOutputFileNameProvider, TStringlyTypedPathOperator>();
+            var serviceAction = new ServiceAction<IHumanOutputFilePathProvider>(() => services.AddCDriveHumanOutputFilePathProvider(addHumanOutputFileNameProvider));
+            return serviceAction;
+        }
+
+        /// <summary>
+        /// Adds a C-drive-based <see cref="IHumanOutputFilePathProvider"/> service using the <see cref="DefaultHumanOutputFileNameProvider"/>.
+        /// </summary>
+        public static IServiceCollection AddCDriveHumanOutputFilePathProvider(this IServiceCollection services)
+        {
+            services.AddCDriveHumanOutputFilePathProvider(services.AddDefaultHumanOutputFileNameProviderAction());
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds a C-drive-based <see cref="IHumanOutputFilePathProvider"/> service using the <see cref="DefaultHumanOutputFileNameProvider"/>.
+        /// </summary>
+        public static ServiceAction<IHumanOutputFilePathProvider> AddCDriveHumanOutputFilePathProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IHumanOutputFilePathProvider>(() => services.AddCDriveHumanOutputFilePathProvider());
+            return serviceAction;
         }
     }
 }
